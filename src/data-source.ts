e9f6044e4dd5c4d49b80bb1ffbfbfa74564c3262
João Borchoski <128744140+JoaoBorchoski@users.noreply.github.com>
2023-05-24 14:19:21 -0300

    fix: appdatasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,40 +1,38 @@
-import "dotenv/config";
-import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
-import path, { join } from "path";
-
-const dataSourceConfig = (): DataSourceOptions => {
-    const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
-    const migrationsPath: string = path.join(
-        __dirname,
-        "./migrations/**.{ts,js}"
-    );
-
-    const dbURL: string | undefined = process.env.DATABASE_URL;
-
-    if (!dbURL) {
-        throw new Error("Env var DATABASE_URL does not exists");
+import path from "path";
+import "dotenv/config";
+const setDataSourceConfig = (): DataSourceOptions => {
+    const entities: string = path.join(__dirname, "./entities/**.{js,ts}");
+    const migration: string = path.join(__dirname, "./migrations/**.{js,ts}");
+    const nodeEnv = process.env.NODE_ENV;
+    if (nodeEnv === "production") {
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL!,
+            entities: [entities],
+            migrations: [migration],
+        };
     }
-
-    const nodeEnv: string | undefined = process.env.NODE_ENV;
-
     if (nodeEnv === "test") {
         return {
             type: "sqlite",
             database: ":memory:",
             synchronize: true,
-            entities: [entitiesPath],
+            entities: [entities],
         };
     }
-
     return {
         type: "postgres",
-        url: dbURL,
+        host: process.env.PGHOST!,
+        username: process.env.PGUSER!,
+        password: process.env.PGPASSWORD!,
+        port: parseInt(process.env.PGPORT!),
+        database: process.env.DB!,
         synchronize: false,
         logging: true,
-        migrations: [migrationsPath],
-        entities: [entitiesPath],
+        entities: [entities],
+        migrations: [migration],
     };
 };
-
-export const AppDataSource = new DataSource(dataSourceConfig());
+const AppDataSource = new DataSource(setDataSourceConfig());
+export default AppDataSource;
