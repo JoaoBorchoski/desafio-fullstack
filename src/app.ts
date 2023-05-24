188db45d966cbcbcabc0e8345bbc957c65064e1c
João Borchoski <128744140+JoaoBorchoski@users.noreply.github.com>
2023-05-24 13:45:40 -0300

    feat: create cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import "express-async-errors";
 import express, { Application } from "express";
 import { handleErrors } from "./errors";
 import { usersRouter, loginRouter, contactRouter } from "./routers";
+import cors from "cors";
 
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/login", loginRouter);
